941ada4f50c9a39ce813da1725b56867198c136f
iceasdza <ice_za.iP@hotmail.com>
2021-01-05 11:05:52 +0700

    [add] Form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, Card, Divider, Input, Space, Typography } from "antd";
+import styled from "styled-components";
 
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  flex-direction: column;
+  width: 100vw;
+  height: 100vh;
+  padding: 16px 24px;
+`;
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Container>
+      <Space>
+        <Input />
+        <Button type="primary">ADD</Button>
+      </Space>
+      <Space direction="vertical" style={{ marginTop: 24 }}>
+        <Card
+          title="TODO 1"
+          style={{ width: 600 }}
+          extra={<Button type="primary">DUPLICATE</Button>}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Space direction="vertical" style={{ width: "100%" }}>
+            <Space>
+              <Input />
+              <Button type="primary">ADD TASK</Button>
+            </Space>
+            <Divider />
+            <Space>
+              <Typography.Text>TASK NAME 1</Typography.Text>
+              <Button type="danger">DELETE</Button>
+              <Button type="primary">DONE</Button>
+            </Space>
+            <Space>
+              <Typography.Text style={{ textDecoration: "line-through" }}>
+                TASK NAME 2
+              </Typography.Text>
+              <Button type="danger">DELETE</Button>
+              <Button type="primary">UNDONE</Button>
+            </Space>
+          </Space>
+        </Card>
+      </Space>
+    </Container>
   );
 }
 
